59b1e307fe38280a850f2f41a1ef8e3b08592633
Nathan Gemmill <nathan.gemmill@itgo.com.au>
2022-03-28 22:08:54 +1100

    this is my first go app

---
+++ src/github.com/nathangemmill/firstapp/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		variable1 int    = 22
+		variable2 int    = 44
+		variable3 string = "Big String"
+	)
+	fmt.Println(variable1, variable2, variable3)
+}
